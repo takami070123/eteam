afa2a302ab752c30fff2f132c8930cb4c0be7157
kanetsuki <r7_kanetsuki@icc.core.ac.jp>
2026-07-16 10:59:00 +0900

    エラーメッセージが表示されるようにしました

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password validations: false
 
-  validates :name, presence: true, uniqueness: true
+
+  validates :name,
+            presence: { message: "を入力してください" }, # 値が入力されているのかチェック
+            uniqueness: { message: "は既に使用されています" } # 値が重複されていないかのチェック
+
+  validates :password,
+            presence: { message: "を入力してください" }, # 値入力チェック
+            length: { minimum: 4, message: "は4文字以上で入力してください" } # 重複チェック
 end
